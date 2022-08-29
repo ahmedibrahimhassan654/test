2ae8cef7eb3f8192a46c3a222fb5f762323aaff6
Ahmed  Ibrahim <ahmedibrahimhassan654@gmail.com>
2022-08-29 14:45:52 +0200

    create read me file and refactor the code

--- index.js
+++ index.js
@@ -3,49 +3,10 @@
 //anshhq@gmail.com
 import findFibs from "./fib.js";
 import getUnique from "./unique.js";
-// //Write a function to find non-repetitive numbers, that are only once in a given array.
-// const Input = [2, 3, 4, 3, 3, 2, 4, 9, 1, 2, 5, 5];
-// // Output: [9, 1]
-// function getUnique(array) {
-//   var l = array.length,
-//     i,
-//     j,
-//     unique = [];
 
-//   for (i = 0; i < l; i++) {
-//     for (j = 0; j < l; j++) {
-//       if (i === j) {
-//         continue;
-//       }
-//       if (array[i] === array[j]) {
-//         break;
-//       }
-//     }
-//     if (j === l) {
-//       unique.push(array[i]);
-//     }
-//   }
-//   return unique;
-// }
+console.log(
+  "The unique numbers in array [2, 3, 4, 3, 3, 2, 4, 9, 1, 2, 5, 5] is =",
+  getUnique([2, 3, 4, 3, 3, 2, 4, 9, 1, 2, 5, 5])
+);
 
-console.log(getUnique([2, 3, 4, 3, 3, 2, 4, 9, 1, 2, 5, 5]));
-
-// Write a function to generate Fibonacci series where maximum number is less than or equal to n.
-// For example, if n is 50 then Fibonacci series is [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
-// For example, if n is 100 then Fibonacci series is [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-// For example, if n is 5 then Fibonacci series is [0, 1, 1, 2, 3, 5]
-// function findFibs(num) {
-
-//     if (num < 2) {
-//       return [1,1];
-//     } else {
-//       var fibs = findFibs(num - 1)
-//       if ((fibs[fibs.length - 1]) < num) {
-//         let toPush = fibs[fibs.length - 1] + fibs[fibs.length - 2];
-//         toPush < num ? fibs.push(toPush) : 0;
-//       }
-//       return fibs;
-//     }
-//   }
-
-console.log(findFibs(100));
+console.log("Fibonacci series for number 100 is =", findFibs(100));
---
+++ readme.md
@@ -0,0 +1,40 @@
+# blockgemini TEST
+
+it's algorithme test for blockgemini interview
+
+## required
+
+1-Write a function to find non-repetitive numbers, that are only once in a given array.
+const Input = [2, 3, 4, 3, 3, 2, 4, 9, 1, 2, 5, 5];
+
+Output: [9, 1]
+
+##
+
+2-Write a function to generate Fibonacci series where maximum number is less than or equal to n.
+For example, if n is 50 then Fibonacci series is [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+For example, if n is 100 then Fibonacci series is [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+For example, if n is 5 then Fibonacci series is [0, 1, 1, 2, 3, 5]
+
+# run the code
+
+Simply, run the following command to run the project dependencies:
+
+```bash
+npm run dev
+```
+
+## Authors
+
+```javascript
+const life: Ahmed Ibrahim = {
+  greet: "Hi 👋, I'm Ahmed Ibrahim MERN Stack WEB Developer  ",
+  bio: 'A passionate engineer love cooding . from Egypt',
+  currentlyLearning: 'advanced full stack udacity nano degree',
+  askMeAbout: 'node js backend +react js +next js ',
+  reachMeAt: 'ahmedibrahimhassan654@gmail.com',
+  funFact: 'love writing code from my deep heart ',
+};
+```
+
+[![Linkedin](https://img.shields.io/badge/LinkedIn-0077B5?style=flat&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/ahmed-ibrahim5588/)
